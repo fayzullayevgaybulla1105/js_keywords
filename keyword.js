f2790710794c317f918598a92bfdf90ccbd2dbdc
fayzullayevgaybulla <fayzullayevgn@gmail.com>
2022-03-25 13:36:40 +0500

    var variable

--- keyword.js
+++ keyword.js
@@ -1,1 +1,35 @@
-    
+/* Javascript variables */
+
+/*------------ var ------------------*/
+
+
+/* o`zgaruvchini var orqali 2xil e`lon qilib olish mumkin. 
+    O`zgaruvchini qiymatlarini ham istalgancha o`zgaritirib ketishimiz mumkin (typeni ahamiyati yo`q). */
+
+var son1 = 50;  // initialization
+var son;    // decloration
+
+console.log(typeof (son))   // undefined
+
+son = 20;   // assignment
+console.log(typeof (son))   // type Number
+
+son = "25"  // re-assignment
+console.log(typeof (son))   // type string
+
+
+var son = [45, 44, 25];   // redecloration
+console.log((son[0]));  // 45
+
+/* var orqali e`lon qilingan o`zgaruvchi global yoki local scopelarga amal qilmaydi. 
+    globalda var orqali e`lon qilingan o`zgaruvchi localda ham shu nom bilan var orqali 
+    e`lon qilingan bo`lsa, localning scopedan keyin ya`ni globalda shu o`zgaruvchini 
+    consolega chiqarganimizda localdagi o`zgaruvchini qiymatini beradi*/
+{
+    {
+        var son1 = 35;
+    }
+}
+console.log(son1);
+
+
