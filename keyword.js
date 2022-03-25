bde689cfedb5c9ce9f7f5bc2c16135ab5cb9c640
fayzullayevgaybulla <fayzullayevgn@gmail.com>
2022-03-25 14:03:20 +0500

    let variable

--- keyword.js
+++ keyword.js
@@ -33,3 +33,60 @@ console.log((son[0]));  // 45
 }
 console.log(son1);
 
+
+/******************************************************************* */
+/*------------ let ------------------*/
+
+
+/* o`zgaruvchini let orqali 2xil e`lon qilib olish mumkin. 
+    O`zgaruvchini qiymatlarini ham istalgancha o`zgaritirib ketishimiz mumkin (typeni ahamiyati yo`q). */
+
+let son2 = 50;  // initialization
+let son3;    // decloration
+
+console.log(typeof (son3))   // undefined
+
+son3 = 20;   // assignment
+console.log(typeof (son3))   // type Number
+
+son3 = "25"  // re-assignment
+console.log(typeof (son3))   // type string
+
+
+
+/* let orqali e`lon qilingan o`zgaruvchi local scope ichida bo`lsa  o`zgaruvchi
+    shu scopening ichida amal qiladi. 
+    globalda let orqali e`lon qilingan o`zgaruvchini, local scopelarga chaqirish mumkin. 
+    lekin localda e`lon qilingan o`zgaruvchini globalda yoki qo`shni scopeda chaqirib bo`lmaydi. 
+    (ichma ich scope bundan mustasno, bu scope ichki scope uchun global hisoblanadi) */
+
+//global
+son3 = '25'
+{
+    //local-global
+    {
+        //local-global
+        {
+            //local
+
+            let son3 = '123456'
+            console.log(son3);
+        }
+        son3 = 41
+        console.log(son3);  //41
+    }
+
+    console.log(son3);  // 41
+
+    son3 = 'olma'
+    console.log(son3);  // olma
+
+}
+console.log(son3);  //olma
+
+/* let orqali bir o`zgaruvchini globalda yoki local scopeda orasida bir xil nom ostida e`lon qilib bo`lmaydi. 
+let raqam = 25;
+let raqam = 41;
+*/
+
+
