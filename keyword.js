a54333e6653bf2e0d21d8e9183c43b04bc8e3c57
fayzullayevgaybulla <fayzullayevgn@gmail.com>
2022-03-25 14:15:11 +0500

    const variable.

--- keyword.js
+++ keyword.js
@@ -97,7 +97,7 @@ let raqam = 41;
 /* const o`zi o`zgarmas degan ma`noni bildiradi. const orqali o`zgaruvchimizga 
         o`zgarmas qiymat berib ketiladi. 
         const decloration qilib keyin qiymat berib ketib bo`lmaydi. 
-        E`lon qilingan o`zgaruvchini qiymatini berib  ketish shart. 
+        E`lon qilingan o`zgaruvchini qiymatini berib ketish shart. 
         uning qiymatini keyin o`zgartirib bo`lmaydi. 
         o`zgarmas deb const bilan e`lon qilib olganimiz uchun */
 
